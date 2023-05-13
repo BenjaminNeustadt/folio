f265fe5870a3ed6a64dcfb9a77a317eb4857eda6
benjamin <b.james.neustadt@gmail.com>
2023-05-13 16:35:52 +0200

    Modify app name for simplicity

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
-class FolioApp < Sinatra::Base
+class Application < Sinatra::Base
+
+  # Allow code to refresh without having to restart server
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    'Hello'
+    'FOLIO'
   end
 
 end
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'rack/test'
+require_relative '../../app'
+
+describe Application do
+  include Rack::Test::Methods
+
+  let(:app) { Application.new }
+
+  context "GET to /" do
+    it "returns 200 OK with the right content" do
+      # Send a GET request to /
+      # and returns a response object we can test.
+      response = get("/")
+
+      # Assert the response status code and body.
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("FOLIO")
+    end
+  end
+end
