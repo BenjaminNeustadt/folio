7fdd855ba9b5a3739977056fd6504c119b42ed36
benjamin <b.james.neustadt@gmail.com>
2023-05-14 22:21:48 +0200

    WIP: configuring the application controller
    * Prepare the templates and organisational structure of the applicatino
    following MVC, by creating an app directory similar to Rails convention.
    * Working with 3 basic views for the moment, whist fleshing out the
    foundations of the application

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require_relative 'app/models/user'
+require_relative 'app/models/image'
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+
+  # def create(new_user)
+  #   encrypted_password = BCrypt::Password.create(new_user.password)
+  #   (email: new_user.email , password: encrypted_password, username: new_user.username, user_id: new_user.user_id)
+  # end
+
+end
 
 class Application < Sinatra::Base
 
@@ -9,8 +21,22 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
+    # new_user = {email: params[:email], password: params[:password], username: params[:username]}
+    # encrypted_password = BCrypt::Password.create(new_user.password)
+    # # User.create(email: 'example@email.com', password: encrypted_password, username: 'Example', user_id: 12 )
+    # User.create(email: new_user.email, password: encrypted_password, username: new_user.username , user_id: 12 )
+    # @all_users = User.all.to_json
+
   get '/' do
-    erb(:index)
+    @users = User.all.to_json
+    erb(:sign_up)
   end
+
+  post '/users/sign_up' do
+    User.create(email: params[:email], password:params[:password] , username: params[:username])
+    redirect '/'
+  end
+
+  # Routes for user signup
 
 end
