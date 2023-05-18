c354c3189899db69a3bdfa0a80aeb9af16a031e6
benjamin <b.james.neustadt@gmail.com>
2023-05-18 22:12:14 +0200

    Add spec for sign_up

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -17,5 +17,15 @@ describe Application do
       expect(response.status).to eq(200)
       # expect(response.body).to eq("FOLIO")
     end
+
+  end
+
+  context 'sign_up' do
+    it 'creates a new account for a user' do
+      response = post '/users/sign_up', { email: 'test@example.com', password: 'password', username: 'user_uno' }
+      # checking for the redirect
+      expect(response.status).to eq(302) # check the response status code
+      expect(User.last.email).to eq('test@example.com') # check that the user was created
+    end
   end
 end
