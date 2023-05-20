44c5c30353ee75c63c33b2c7d46475b39766fde0
benjamin <b.james.neustadt@gmail.com>
2023-05-21 00:35:50 +0200

    WIP: extract user model into models
    * Begin extaction into MVC structure

--- app.rb
+++ app.rb
@@ -16,24 +16,6 @@ require 'net/http'
 require 'mapkick'
 
 
-class User < ActiveRecord::Base
-  has_many :images, dependent: :destroy
-
-  before_save :encrypt_password
-
-  private
-
-  def encrypt_password
-    self.password = BCrypt::Password.create(password)
-  end
-
-  public
-
-  def authenticate(password)
-    BCrypt::Password.new(self.password) == password
-  end
-
-end
 
 class Image < ActiveRecord::Base
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  has_many :images, dependent: :destroy
+  before_save :encrypt_password
+
+  private
+
+  def encrypt_password
+    self.password = BCrypt::Password.create(password)
+  end
+
+  public
+
+  def authenticate(password)
+    BCrypt::Password.new(self.password) == password
+  end
+
 end
