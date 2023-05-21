a4aac4e8386bb7c7c352df2ffcca3301e3dba9d5
benjamin <b.james.neustadt@gmail.com>
2023-05-21 16:05:59 +0200

    Extact logic to Controller modules
    * Currently the UserController and ImageController are both modules,
    for convenience. But they should become classes at some stage.

--- app.rb
+++ app.rb
@@ -13,38 +13,18 @@ require 'exif'
 require 'net/http'
 require 'mapkick'
 
-class Application < Sinatra::Base
-  instance_eval(File.read('config/config.rb'))
 
-  # the order of these is important
-  enable :sessions
-  register Sinatra::Flash
-  register Sinatra::Partial
+module UserController
 
-  enable :partial_underscores
-  set :partial_template_engine, :erb
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
 
-  # :TODO: maybe just put these inside their respetive models,
-  # since these are interacting with the database so that's where they should be.
-  before do
-    @current_user = User.find(session[:user_id]) if session[:user_id]
-    @current_page = request.path_info
-    @images = Image.all
+  def all_users
     @users = User.all
-    @bucket = settings.s3.bucket('folio-test-bucket')
-    @bucket_objects = @bucket.objects.to_a rescue []  # rescue empty array if bucket does not exist or is empty
   end
 
-  get '/' do
-    # @current_page = '/'
-    # @user  = session[:user_id]
-    # @users = User.all.to_json
-    # @users = User.all
-    erb(:sign_up)
-  end
-
-  # :TODO: put the User.create inside the model also
-  post '/users/sign_up' do
+  def sign_up_user
     User.create(
       email: params[:email],
       password: params[:password], 
@@ -52,8 +32,8 @@ class Application < Sinatra::Base
     )
     redirect '/'
   end
-
-  post '/users/sign_in' do
+  
+  def sign_in_user
     user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
@@ -66,9 +46,13 @@ class Application < Sinatra::Base
     end
   end
 
-  # search for an indepenedent user's feed/page
-  get '/users/:username' do
-    # @current_page = '/users/:username'
+  def logout_current_user
+    session.clear
+    flash[:notice] = "Until the next..."
+    redirect '/'
+  end
+
+  def search_user
     @user = User.find_by(username: params[:username])
 
     if @user
@@ -80,35 +64,52 @@ class Application < Sinatra::Base
     end
   end
 
-  # This is the special account page
-  get '/account_page' do
-    # @current_page = '/account_page'
+  def user_has_session?
     if session[:user_id].nil?
       return redirect('/')
     else
       @user  = session[:user_id]
-      # @current_user = User.find(session[:user_id])
-      # @images = Image.all
-      # @bucket = settings.s3.bucket('folio-test-bucket')
-      # @bucket_objects = @bucket.objects.to_a rescue []  # rescue empty array if bucket does not exist or is empty
-      # @users = User.all.to_json
-      # @users = User.all
       return erb(:account_page)
     end
   end
 
-  # :TODO: put this in ExifHelpers module
-  def convert_gps_coordinates(coordinate)
-    degrees = coordinate[0].numerator.to_f / coordinate[0].denominator
-    minutes = coordinate[1].numerator.to_f / coordinate[1].denominator / 60
-    seconds = coordinate[2].numerator.to_f / coordinate[2].denominator / 3600
-    
-    decimal_coordinate = degrees + minutes + seconds
-    decimal_coordinate
+end
+
+module ImageController
+
+  def all_images
+    Image.all
   end
 
-  # IMAGE UPLOAD
-  post '/upload' do
+  def display_map_page
+    @test_image = Image.all.last
+    erb(:map_page)
+  end
+
+  def image_data_to_json
+    content_type :json
+
+    images_data = []
+
+    images = Image.all
+    images.each do |image|
+    image_link = "<img src='" + image.url + "' style='max-width: 200px; max-height: 200px;'>"
+      images_data << {
+        latitude: image.gps_latitude,
+        longitude: image.gps_longitude,
+        label: image.caption,
+        tooltip: image_link
+      }
+    end
+    images_data.to_json
+  end
+
+  def delete_image
+    Image.find(params[:id]).destroy
+    redirect '/account_page'
+  end
+
+  def upload_image
     # Get the user_id from the session
     user_id = session[:user_id]
     # get the file
@@ -139,45 +140,65 @@ class Application < Sinatra::Base
     redirect '/account_page'
   end
 
+  # :TODO: put this in ExifHelpers module
+  def convert_gps_coordinates(coordinate)
+    degrees = coordinate[0].numerator.to_f / coordinate[0].denominator
+    minutes = coordinate[1].numerator.to_f / coordinate[1].denominator / 60
+    seconds = coordinate[2].numerator.to_f / coordinate[2].denominator / 3600
+    
+    decimal_coordinate = degrees + minutes + seconds
+    decimal_coordinate
+  end
+
+end
+
+class Application < Sinatra::Base
+  instance_eval(File.read('config/config.rb'))
+
+  include UserController
+  include ImageController
+
+  enable :sessions
+  register Sinatra::Flash
+  register Sinatra::Partial
+
+  enable :partial_underscores
+  set :partial_template_engine, :erb
+
+  # create instance variables to pass to views
+  before do
+    @current_user = current_user
+    @current_page = request.path_info
+    @images = all_images
+    @users = all_users
+    @bucket = settings.s3.bucket('folio-test-bucket')
+    @bucket_objects = @bucket.objects.to_a rescue []  # rescue empty array if bucket does not exist or is empty
+  end
+
+  get '/' do
+    erb(:sign_up)
+  end
+
+  post('/users/sign_up') { sign_up_user } 
+
+  # search for an indepenedent user's feed/page
+  post('/users/sign_in') { sign_in_user }
+
+  get('/users/:username') { search_user }
+
+  get('/account_page') {user_has_session?}
+
+  post('/upload') { upload_image }
+
   # :TODO: use 'delete' instead of 'post'
-  # deleting an image
-  post '/images/:id' do
-    Image.find(params[:id]).destroy
-    redirect '/account_page'
-  end
+  post('/images/:id') {delete_image}
 
-  get '/images_data.json' do
-    content_type :json
+  get('/images_data.json') {image_data_to_json}
 
-    images_data = []
+  get('/map_page') {display_map_page}
 
-    images = Image.all
-    images.each do |image|
-    image_link = "<img src='" + image.url + "' style='max-width: 200px; max-height: 200px;'>"
-      images_data << {
-        latitude: image.gps_latitude,
-        longitude: image.gps_longitude,
-        label: image.caption,
-        tooltip: image_link
-      }
-    end
-    images_data.to_json
-  end
+  get('/logout') {logout_current_user}
 
-  get '/map_page' do
-    @test_image = Image.all.last
-    # @current_page = '/map_page'
-    erb(:map_page)
-  end
-
-  get '/logout' do
-    session.clear
-    flash[:notice] = "Until the next..."
-    redirect '/'
-  end
-
-  get '/shop_page' do
-    erb(:shop_page)
-  end
+  get('/shop_page') {erb(:shop_page)}
 
 end
