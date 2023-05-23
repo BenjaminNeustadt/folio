9a902e282dc9b949095dfb9e0ded7da916e6d21a
benjamin <b.james.neustadt@gmail.com>
2023-05-23 16:11:03 +0200

    Spec update helper
    * Arrange and order

--- db/migrate/20230515094311_new_schema.rb
+++
@@ -1,9 +0,0 @@
-class NewSchema < ActiveRecord::Migration[7.0]
-  def change
-    create_table "users", force: :cascade do |t|
-      t.string "username", null: false
-      t.string "email", null: false
-      t.string "password", null: false
-    end
-  end
-end
