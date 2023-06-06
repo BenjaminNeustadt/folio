b8b789df8705435b55015d43899abf367699e9e2
benjamin <b.james.neustadt@gmail.com>
2023-06-06 16:46:35 +0200

    Extract script switching modal views to public
    * Set the visibility of following to display: none in order to have the
    section intially obfuscated from view, as previously the JS was
    responsible for hiding it upon load, and now it is rendered after the
    html as opposed to with it.

---
+++ public/account_page_display_modals.js
@@ -0,0 +1,29 @@
+ function showImages() {
+    const imageSection = document.querySelector('#image-section');
+    const mapSection = document.querySelector('#map-section');
+    const followingSection = document.querySelector('#following-section');
+
+    imageSection.style.display = 'block';
+    mapSection.querySelector('#your-hidden-map-container').id = 'your-visible-map-container';
+    followingSection.style.display = 'none';
+  }
+
+  function showMap() {
+    const imageSection = document.querySelector('#image-section');
+    const mapSection = document.querySelector('#map-section');
+    const followingSection = document.querySelector('#following-section');
+
+    imageSection.style.display = 'none';
+    mapSection.querySelector('#your-hidden-map-container').id = 'your-visible-map-container';
+    followingSection.style.display = 'none';
+  }
+
+  function showFollowing() {
+    const imageSection = document.querySelector('#image-section');
+    const mapSection = document.querySelector('#map-section');
+    const followingSection = document.querySelector('#following-section');
+
+    imageSection.style.display = 'none';
+    mapSection.querySelector('#your-visible-map-container').id = 'your-hidden-map-container';
+    followingSection.style.display = 'block';
+  }
