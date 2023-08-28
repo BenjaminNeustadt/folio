de74e9016b4da5cadeda48f5e4b87f68d30aa0ae
benjamin <b.james.neustadt@gmail.com>
2023-08-28 15:16:09 +0200

    Include a sandbox file for playing around
    This sandbox was specifically created for sendgrid

--- sandbox.rb
+++ sandbox.rb
@@ -20,13 +20,34 @@ require 'sendgrid-ruby'
 require 'dotenv/load'
 include SendGrid
 
+# user = User.find_by(email: params[:email])
 from = Email.new(email: 'b.james.neustadt@gmail.com')
 to = Email.new(email: 'b.james.neustadt@gmail.com')
 subject = 'Sending with SendGrid is Fun'
 
-content = "Click on this button to verify your account #{button}"
+# content = "Click on this button to verify your account #{button}"
 
-content = Content.new(type: 'text/plain', value: 'Thank you for your service')
+user = "Benjamin"
+
+email_body =
+ <<~HTML
+<p>Hello #{user},</p>
+<p>Click the button below to verify your account:</p>
+<a href="http:localhost:3000/" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px;">Verify Account</a>
+HTML
+
+# going to button i
+
+get('/verification_of_user/:user_id'){
+  verify_user(params[:user_id])
+}
+
+def verify_user(user_id)
+  user = User.find(user_id).update
+  user.update.verified_account
+end
+
+content = Content.new(type: 'text/html', value: email_body )
 mail = Mail.new(from, subject, to, content)
 
 puts ENV['SENDGRID_API_KEY']
